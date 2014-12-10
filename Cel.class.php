d4d06c309217ec9455d90d646a45b97f88906b3e
Jason Parker <jason.parker@schmoozecom.com>
2014-12-10 10:48:59 -0600

    Initial commit

---
+++ Cel.class.php
@@ -0,0 +1,25 @@
+<?php
+// vim: set ai ts=4 sw=4 ft=php:
+//	License for all code of this FreePBX module can be found in the license file inside the module directory
+//	Copyright 2014 Schmooze Com Inc.
+//
+namespace FreePBX\modules;
+class Cel extends \FreePBX_Helpers implements \BMO {
+	public function __construct($freepbx = null) {
+		$this->db = $freepbx->Database;
+		$this->freepbx = $freepbx;
+	}
+
+	public function install() {
+
+	}
+	public function uninstall() {
+
+	}
+	public function backup(){
+
+	}
+	public function restore($backup){
+
+	}
+}
