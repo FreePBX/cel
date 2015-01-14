14dd7c2698d19e2059ae25da3033bd4b5f799db5
Jason Parker <jason.parker@schmoozecom.com>
2015-01-14 15:58:04 -0600

    Attempt to transparently handle local channels, when needed.

--- Cel.class.php
+++ Cel.class.php
@@ -48,7 +48,7 @@ class Cel extends \FreePBX_Helpers implements \BMO {
 		);
 		$badcontexts = array('tc-maint');
 
-		$sql = "SELECT " . implode(", ", $fields) . " FROM cel WHERE context NOT IN ('" . implode("', '", $badcontexts) . "')";
+		$sql = "SELECT " . implode(", ", $fields) . " FROM cel WHERE context NOT IN ('" . implode("', '", $badcontexts) . "') ORDER BY id";
 		$res = $cdrdb->getAll($sql, DB_FETCHMODE_ASSOC);
 
 		$channels = array();
@@ -57,6 +57,19 @@ class Cel extends \FreePBX_Helpers implements \BMO {
 
 			switch ($row['eventtype']) {
 			case 'CHAN_START':
+				if (substr($row['channame'], 0, 6) == "Local/") {
+					/* Ugh... */
+					$mapname = substr($row['channame'], 0, -2);
+					if (substr($row['channame'], -2, 2) == ";1") {
+						if ($row['uniqueid'] != $row['linkedid']) {
+							$localmaps[$mapname]['owner'] = $row['linkedid'];
+						}
+						$localmaps[$mapname]['one'] = $row['uniqueid'];
+					} else {
+						$localmaps[$mapname]['two'] = $row['uniqueid'];
+					}
+				}
+
 				/* New channel! */
 				$channels[$row['uniqueid']]['starttime'] = new \DateTime($row['eventtime']);
 				$channels[$row['uniqueid']]['cid_num'] = $row['cid_num'];
@@ -69,16 +82,27 @@ class Cel extends \FreePBX_Helpers implements \BMO {
 				if ($row['uniqueid'] == $row['linkedid']) {
 					$calls[$row['uniqueid']]['starttime'] = new \DateTime($row['eventtime']);
 					$calls[$row['uniqueid']]['cid_num'] = $row['cid_num'];
+					$calls[$row['uniqueid']]['extension'] = $row['exten'];
 				}
 				break;
 			case 'ANSWER':
 				$channels[$row['uniqueid']]['answertime'] = new \DateTime($row['eventtime']);
 				break;
 			case 'BRIDGE_ENTER':
-				$bridges[$extra['bridge_id']][$row['uniqueid']]['entertime'] = new \DateTime($row['eventtime']);
+				if (($localmap = $localmaps[substr($row['channame'], 0, -2)]) && $row['uniqueid'] == $localmap['two']) {
+					$uniqueid = $localmap['owner'];
+				} else {
+					$uniqueid = $row['uniqueid'];
+				}
+				$bridges[$extra['bridge_id']][$uniqueid]['entertime'] = new \DateTime($row['eventtime']);
 				break;
 			case 'BRIDGE_EXIT':
-				$bridges[$extra['bridge_id']][$row['uniqueid']]['exittime'] = new \DateTime($row['eventtime']);
+				if (($localmap = $localmaps[substr($row['channame'], 0, -2)]) && $row['uniqueid'] == $localmap['two']) {
+					$uniqueid = $localmap['owner'];
+				} else {
+					$uniqueid = $row['uniqueid'];
+				}
+				$bridges[$extra['bridge_id']][$uniqueid]['exittime'] = new \DateTime($row['eventtime']);
 				break;
 			case 'BLINDTRANSFER':
 				$calls[$extra['transferee_channel_uniqueid']]['actions'][] = array(
@@ -110,7 +134,6 @@ class Cel extends \FreePBX_Helpers implements \BMO {
 
 				if ($row['uniqueid'] == $row['linkedid']) {
 					$calls[$row['uniqueid']]['endtime'] = new \DateTime($row['eventtime']);
-					$calls[$row['uniqueid']]['extension'] = $channels[$row['uniqueid']]['extension'];
 				}
 				break;
 			case 'LINKEDID_END':
@@ -121,6 +144,14 @@ class Cel extends \FreePBX_Helpers implements \BMO {
 
 		foreach ($channels as $uniqueid => $channel) {
 			if ($channel['linkedid']) {
+				if (($localmap = $localmaps[substr($channel['channel'], 0, -2)]) && $localmap['owner'] == $channel['linkedid']) {
+					continue;
+				}
+
+				if (!$channel['answertime']) {
+					continue;
+				}
+
 				/* This channel is part of another call. */
 				$callid = $channel['linkedid'];
 
@@ -139,16 +170,20 @@ class Cel extends \FreePBX_Helpers implements \BMO {
 				$call = $calls[$callid];
 
 				foreach ($bridges as $bridgeid => $bridge) {
-					if (isset($bridge[$uniqueid])) {
+					if (isset($bridge[$callid])) {
 						$action = array(
 							'type' => 'bridge',
-							'starttime' => $bridge[$uniqueid]['entertime'],
-							'stoptime' => $bridge[$uniqueid]['exittime'],
+							'starttime' => $bridge[$callid]['entertime'],
+							'stoptime' => $bridge[$callid]['exittime'],
 							'bridge' => $bridgeid,
 						);
 
 						foreach ($bridge as $linkid => $link) {
-							if ($linkid == $uniqueid) {
+							if ($linkid == $callid) {
+								continue;
+							}
+
+							if (($localmap = $localmaps[substr($channels[$linkid]['channel'], 0, -2)]) && $localmap['owner'] == $channels[$linkid]['linkedid']) {
 								continue;
 							}
 
@@ -164,7 +199,9 @@ class Cel extends \FreePBX_Helpers implements \BMO {
 
 						}
 
-						$call['actions'][] = $action;
+						if (count($action['members']) > 0) {
+							$call['actions'][] = $action;
+						}
 					}
 				}
 			}
