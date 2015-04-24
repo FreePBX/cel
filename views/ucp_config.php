1e4737b7df3b624b5468cc7f39e6ec1f3e3f4532
Andrew Nagy <andrew.nagy@the159.com>
2015-04-23 20:22:11 -0400

    formatting

--- views/ucp_config.php
+++ views/ucp_config.php
@@ -4,8 +4,8 @@
 			<div class="row">
 				<div class="form-group">
 					<div class="col-md-3">
-						<label class="control-label" for="cel_download"><?php echo _("Allow CEL")?></label>
-						<i class="fa fa-question-circle fpbx-help-icon" data-for="cel_download"></i>
+						<label class="control-label" for="cel_enable"><?php echo _("Allow CEL")?></label>
+						<i class="fa fa-question-circle fpbx-help-icon" data-for="cel_enable"></i>
 					</div>
 					<div class="col-md-9">
 						<span class="radioset">
@@ -21,7 +21,7 @@
 	</div>
 	<div class="row">
 		<div class="col-md-12">
-			<span id="cdr_enable-help" class="help-block fpbx-help-block"><?php echo _("Enable CEL (Call Event Logging) in UCP for this user")?></span>
+			<span id="cel_enable-help" class="help-block fpbx-help-block"><?php echo _("Enable CEL (Call Event Logging) in UCP for this user")?></span>
 		</div>
 	</div>
 </div>
