632e9e64329b92524a6efe918c216c280adf7ecb
Philip Joseph <jphilip@sangoma.com>
2025-12-16 02:23:47 -0500

    FREEI-2509: Fix Whoops undefined array key error

--- Cel.class.php
+++ Cel.class.php
@@ -157,7 +157,7 @@ class Cel extends \FreePBX_Helpers implements \BMO {
 			$data = $this->FreePBX->Core->getDevice($defaultexten);
 			if(empty($data) || empty($data['description'])) {
 				$data = $this->FreePBX->Core->getUser($defaultexten);
-				$name = $data['name'];
+				$name = isset($data['name']) ? $data['name'] : (isset($data['username']) ? $data['username'] : $_POST['username']);
 			} else {
 				$name = $data['description'];
 			}
